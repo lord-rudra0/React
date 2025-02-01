68c6dc6266b362b94c08e6a2bab71a7829b4c6bb
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-02 00:32:03 +0530

    var use in js

--- intro_jsx/src/index.js
+++ intro_jsx/src/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+// use of render
+
+// ReactDOM.render(
+//   <div>
+//     <h1>this is h1</h1>
+//     <ul>
+//       <li>list1</li>
+//       <li>list2</li>
+//       <li>list3</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+
+// use of variable in js code 
+
+
+const name = "rudra";
+const lucy = 17;
+
 ReactDOM.render(
   <div>
-    <h1>udi2hdwuhudb2</h1>
-    <ul>
-      <li>jdisjd</li>
-      <li>okd2oe</li>
-      <li>jqwdnudhu</li>
-    </ul>
+    <h1>Hello {name} </h1>
+    <p1>My lucky number is {lucy}</p1>
   </div>,
   document.getElementById("root")
 );
