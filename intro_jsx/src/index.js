0d7b36ba5de89bc6af7669dcf996f35ad998f51e
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-02 00:19:13 +0530

    end task 1

---
+++ intro_jsx/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>udi2hdwuhudb2</h1>
+    <ul>
+      <li>jdisjd</li>
+      <li>okd2oe</li>
+      <li>jqwdnudhu</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
