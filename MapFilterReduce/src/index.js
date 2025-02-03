b3d4d86366cd89d5a151ba2791159439f7c91b13
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-03 18:51:39 +0530

    substring

--- MapFilterReduce/src/index.js
+++ MapFilterReduce/src/index.js
@@ -1,15 +1,15 @@
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
 
 
 
 
-// using forEach() MAP
+// using forEach=== MAP
 // let newNumber=[];
-// function dble(num){
+// function double(num){
 //     newNumber.push(num*2);
 // }
-// numbers.forEach(dble);
+// numbers.forEach(double);
 // console.log(newNumber);
 
 // numbers.forEach((num)=>
@@ -18,8 +18,8 @@ var numbers = [3, 56, 2, 48, 5];
 // });
 // console.log(newNumber);
 
-
-// let newNumber=[];//reduce
+// FILTER ===foreach
+// let newNumber=[];
 
 // numbers.forEach(function(num){
 //     // return newNumber.push(num%2===0);
@@ -28,6 +28,13 @@ var numbers = [3, 56, 2, 48, 5];
 // });
 // console.log(newNumber);
 
+
+// REDUCE===foreach
+// let newNumber=0;
+// numbers.forEach(function(num){
+// newNumber+=num
+// })
+// console.log(newNumber);
 
 
 
@@ -60,8 +67,36 @@ var numbers = [3, 56, 2, 48, 5];
 
 
 //Reduce - Accumulate a value by doing something to each item in an array.
+// let newNumber=numbers.reduce(function(num,sum){
+//     console.log(num);
+//     console.log("sum is ",sum);
+//     return num+sum;
+// })
+// console.log(newNumber);
+
 
 
 //Find - find the first item that matches from an array.
+// const newNumber=numbers.find((num)=>
+// {
+//     return num>500;
+// })
+// console.log(newNumber);
 
 //FindIndex - find the index of the first item that matches.
+// const newNumber=numbers.findIndex((num)=>
+//     {
+//         return num===48;
+//     })
+//     console.log(newNumber);
+
+
+import emojipedia from "./emojipedia";
+
+
+const mean=emojipedia.map(function(mean){
+return mean.meaning.substring(0,99)
+});
+console.log(mean)
+
+console.log(mean);
