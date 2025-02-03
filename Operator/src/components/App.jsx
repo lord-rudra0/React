fc1cf8cc45f289ca802334332b1259416926fbe7
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-03 23:39:11 +0530

    Ternary Op

--- Operator/src/components/App.jsx
+++ Operator/src/components/App.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import Login from "./login";
 // import Input from "./input";
 
-const is_login=true;
+const is_login=false;
 
-function isLogin(){
-  if(is_login){
-    return <h1>Hello</h1>
-  }
-   return <Login />  
-}
+// function isLogin(){
+//   if(is_login){
+//     return <h1>Hello</h1>
+//   }
+//    return <Login />  
+// }
 
+const current_time=new Date().getHours();
+console.log(current_time)
 function App() {
   return (
     <div className="container">
       {/* <h1>Hello</h1>
       <Login /> */}
-      {isLogin()}
+      {/* {isLogin()} */}
+      {is_login?<h1>Hello</h1>:<Login />}
+      {/* {current_time>9?<h1>Class Time</h1>:null} */}
+     {/* { current_time>9&&<h1>Class Time</h1>} */}
     </div>
   );
 }
