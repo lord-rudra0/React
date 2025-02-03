b0a1127e6b9ac14efde12577736077c07dc88500
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-03 19:21:29 +0530

    arrow use in desc

--- Arrow Function/src/index.js
+++ Arrow Function/src/index.js
@@ -4,29 +4,47 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+// const numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
 // const newNumbers = numbers.map(function (x) {
 //   return x * 2;
 // });
+// const newNumber =numbers.map(x=>x*2);
+// console.log(newNumber)
+
+
 
 //////Filter - Create a new array by keeping the items that return true.
 // const newNumbers = numbers.filter(function(num) {
 //   return num < 10;
 // });
+// const newNumber=numbers.filter(num=>num<10);
+// console.log(newNumber)
+
+
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 // var newNumber = numbers.reduce(function (accumulator, currentNumber) {
 //     return accumulator + currentNumber;
 // })
+// const newNumber=numbers.reduce((sum,num)=>sum+num);
+// console.log(newNumber)
+
+
 
 ////Find - find the first item that matches from an array.
 // const newNumber = numbers.find(function (num) {
 //   return num > 10;
 // })
+// const newNumber=numbers.find(num=>num>10);
+// console.log(newNumber)
+
+
 
 ////FindIndex - find the index of the first item that matches.
 // const newNumber = numbers.findIndex(function (num) {
 //   return num > 10;
 // })
+// const newNumber=numbers.findIndex(num=>num>10);
+// console.log(newNumber)
