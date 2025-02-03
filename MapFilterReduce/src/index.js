19434201ba188ff8edec38b6ef8b0af4fe8745c6
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-03 18:18:36 +0530

    map and reduce and forEach

---
+++ MapFilterReduce/src/index.js
@@ -0,0 +1,67 @@
+var numbers = [3, 56, 2, 48, 5];
+
+
+
+
+
+// using forEach() MAP
+// let newNumber=[];
+// function dble(num){
+//     newNumber.push(num*2);
+// }
+// numbers.forEach(dble);
+// console.log(newNumber);
+
+// numbers.forEach((num)=>
+// {
+//     newNumber.push(num*2);
+// });
+// console.log(newNumber);
+
+
+// let newNumber=[];//reduce
+
+// numbers.forEach(function(num){
+//     // return newNumber.push(num%2===0);
+//     if(num%2===0)
+//         newNumber.push(num);
+// });
+// console.log(newNumber);
+
+
+
+
+
+            //Map -Create a new array by doing something with each item in an array.
+//  function double(num){
+//     return num*2;
+//  }
+
+// const new_map= numbers.map(double);
+// console.log(new_map);
+
+// const newNumber= numbers.map((num)=>
+// {
+//    return (num*2);
+// })
+// console.log(newNumber);
+
+
+
+
+
+            //Filter - Create a new array by keeping the items that return true.
+// const newNumber=numbers.filter(function(num){
+//     return num%2===0;
+// });
+// console.log(newNumber);
+
+
+
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
