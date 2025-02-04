13d1bc3ea2de23e22250f63cfd950c100186a715
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-04 19:23:11 +0530

    use state

--- Hook State/src/components/App.jsx
+++ Hook State/src/components/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React ,{ useState } from "react";
+
+
 
 function App() {
-  return <div />;
+// let num=0;
+// const state= useState();
+const [count,setCount]=useState(0)
+console.log(count);
+
+function increase()
+{
+  setCount(count+1)
+  // num++;
+    // <div className="container">
+    //   <h1>{num}</h1>
+    //   <button onClick={increase} type="button">+</button>
+    // </div>
+  
+}
+
+// console.log("I got clicked",num);
+
+  return  ( <div className="container">
+  <h1>{count}</h1>
+  <button onClick={increase} type="button">+</button>
+</div>);
 }
 
 export default App;
--- Hook State/src/index.js
+++ Hook State/src/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App"
 
 ReactDOM.render(
-  <div className="container">
-    <h1>0</h1>
-    <button>+</button>
-  </div>,
+<App />,
   document.getElementById("root")
 );
+
+
+// let num=0;
+
+// function increase()
+// {
+//   num++;
+//   // console.log("I got clicked",num);
+//   ReactDOM.render(
+//     <div className="container">
+//       <h1>{num}</h1>
+//       <button onClick={increase} type="button">+</button>
+//     </div>,
+//     document.getElementById("root")
+//   );
+// }
+
+
+
+// ReactDOM.render(
+//   <div className="container">
+//     <h1>{num}</h1>
+//     <button onClick={increase} type="button">+</button>
+//   </div>,
+//   document.getElementById("root")
+// );
+
