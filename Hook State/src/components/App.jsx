d860dadcf8ba9a82c5fecd9f688af71950a3ebfa
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-04 19:38:08 +0530

    rewrite everything

--- Hook State/src/components/App.jsx
+++ Hook State/src/components/App.jsx
@@ -1,34 +1,86 @@
-import React ,{ useState } from "react";
+// import React ,{ useState } from "react";
 
 
 
-function App() {
-// let num=0;
-// const state= useState();
-const [count,setCount]=useState(0)
-console.log(count);
+// function App() {
+// // let num=0;
+// // const state= useState();
+// const [count,setCount]=useState(0)
+// console.log(count);
 
-function increase()
-{
-  setCount(count+1)
-  // num++;
-    // <div className="container">
-    //   <h1>{num}</h1>
-    //   <button onClick={increase} type="button">+</button>
-    // </div>
+// function increase()
+// {
+//   setCount(count+1)
+//   // num++;
+//     // <div className="container">
+//     //   <h1>{num}</h1>
+//     //   <button onClick={increase} type="button">+</button>
+//     // </div>
   
-}
+// }
 
-function decrease(){
-  setCount(count-1)
-}
-// console.log("I got clicked",num);
+// function decrease(){
+//   setCount(count-1)
+// }
+// // console.log("I got clicked",num);
 
-  return  ( <div className="container">
-  <h1>{count}</h1>
-  <button onClick={increase} type="button">+</button>
-  <button onClick={decrease} type="button">-</button>
-</div>);
+//   return  ( <div className="container">
+//   <h1>{count}</h1>
+//   <button onClick={increase} type="button">+</button>
+//   <button onClick={decrease} type="button">-</button>
+// </div>);
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React ,{useState} from "react";
+
+
+function App()
+{
+  const [count,setState]=useState(0)
+
+  function increase()
+  {
+    setState(count+1);
+
+  }
+
+  function decrease()
+  {
+    setState(count-1);
+    
+  }
+
+  return <div>
+    <h1>{count}</h1>
+    <button type="button" onClick={increase}>+</button>
+    <button type="button" onClick={decrease}>-</button>
+  </div>
 }
 
 export default App;
