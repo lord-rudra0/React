cff1837cc4e375b4ae1b206e01953560beb11928
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-04 22:47:11 +0530

    destructuring

--- React Hook Practice/src/components/App.jsx
+++ React Hook Practice/src/components/App.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function App() {
+  let curr_time = new Date().toLocaleTimeString();
+  console.log(curr_time)
+  const [time,getTime]=useState(curr_time);
+  function findTime()
+  {
+    
+    getTime(time)
+  }
+
+  function Refresh_Page()
+  {
+    window.location.reload();
+    console.log("reloaded")
+
+  }
+  
+// console.log(time);
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      {setInterval(Refresh_Page,500000)}
+      <button onClick={findTime} type="button">Get Time</button>
     </div>
   );
 }
