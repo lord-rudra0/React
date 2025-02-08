c13880b0fddcb3895d1b5de5a3710b28bffdb86b
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-08 22:35:54 +0530

    preventDefault

--- Forms/src/components/App.jsx
+++ Forms/src/components/App.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React ,{useState} from "react";
 
 function App() {
+
+  const [Username,getUsername]=useState("");
+  const [name,setName]=useState("");
+
+  function HandleEvent(e)
+  {
+    console.log(e.target.value);
+    getUsername(e.target.value);
+    // const Username=e.target.value
+    // console.log(Username)
+  }
+
+  function getName(e)
+  {
+    console.log("Submitted")
+    // getUsername(e.target.value)
+    setName(Username)
+
+
+    e.preventDefault();
+
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {name} </h1>
+      <form>
+      <input
+          value={Username}
+          onChange={HandleEvent} 
+            // onClick={getName} 
+          // onSubmit={getName}
+          // onClick={getName}
+          type="text" 
+          placeholder="What's your name?" />
+      <button  type="button" onClick={getName}>Submit</button>
+      </form>
+      {/* <input
+          value={Username}
+          onChange={HandleEvent} 
+            // onClick={getName} 
+          // onSubmit={getName}
+          // onClick={getName}
+          type="text" 
+          placeholder="What's your name?" />
+      <button  type="button" onClick={getName}>Submit</button> */}
     </div>
   );
 }
