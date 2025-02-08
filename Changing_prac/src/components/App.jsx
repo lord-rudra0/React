b5ef6e7e9ec09af657b0fffbc8edbbe9e02598eb
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-09 01:50:37 +0530

    end

--- Changing_prac/src/components/App.jsx
+++ Changing_prac/src/components/App.jsx
@@ -7,6 +7,55 @@ function App() {
     email: ""
   });
 
+  function InputHandle(e)
+  {
+    const inputValue=e.target.value;
+    console.log(inputValue)
+    const inputName=e.target.name;
+    console.log(inputName)
+
+    setContact((prev)=>
+    {
+      if(inputName==="fName")
+      {
+        return{
+            fName:inputValue,
+            lName:prev.lName,
+            email:prev.email
+        };
+      }
+
+      else if(inputName==="lName")
+        {
+          return{
+              fName:prev.fName,
+              lName:inputValue,
+              email:prev.email
+          };
+        }
+
+
+        else if(inputName==="email")
+          {
+            return{
+                fName:prev.fName,
+                lName:prev.lName,
+                email:inputValue
+            };
+          }
+
+    })
+
+    // CHATGPT METHOD
+
+    const { name, value } = e.target;
+    setContact((prev) => ({
+      ...prev,
+      [name]: value // Dynamically updates the correct field
+    }));
+
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,10 +63,13 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
-        <button>Submit</button>
+        <input name="fName" placeholder="First Name" 
+        value={contact.fName} onChange={InputHandle} />
+        <input name="lName" placeholder="Last Name" 
+        value={contact.lName} onChange={InputHandle} />
+        <input name="email" placeholder="Email"
+         value={contact.email}onChange={InputHandle} />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
